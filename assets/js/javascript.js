f7aed98b032e1eabc7f41112c2bedddf521a531c
David Bernard <dbernardcoding@gmail.com>
2021-03-29 18:32:03 -0400

    adding questions to javascript

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,34 @@
 let submitBtn = document.querySelector(".btn");
+let choiceOne = document.querySelector("#choiceOne");
+let choiceTwo = document.querySelector("#choiceTwo");
+let choiceThree = document.querySelector("#choiceThree");
+let choiceFour = document.querySelector("#choiceFour");
 
 submitBtn.setAttribute("style", "padding: 10px 20px; color:blue; background-color:coral; color:whitesmoke; border-radius:5px");
+choiceOne.setAttribute("style", "padding: 10px 20px; color:blue; background-color:coral; color:whitesmoke; border-radius:5px");
+choiceTwo.setAttribute("style", "padding: 10px 20px; color:blue; background-color:coral; color:whitesmoke; border-radius:5px");
+choiceThree.setAttribute("style", "padding: 10px 20px; color:blue; background-color:coral; color:whitesmoke; border-radius:5px");
+choiceFour.setAttribute("style", "padding: 10px 20px; color:blue; background-color:coral; color:whitesmoke; border-radius:5px");
+
+let quizQuestions = [
+    {
+        question: "Which pair of characters are used to create an array in Javascript?",
+        choice: ["<>", "()", "[]", "''"],
+        answer: "[]",
+    },
+    {
+        question: "Which attribute is used to change font color in Javascript?",
+        choice: ["style", "set", "assign", "change"],
+        answer: "style",
+    },
+    {
+        question: "Which set of characters are used to assign AND to an if else statement?",
+        choice: ["!!", "||", "++", "&&"],
+        answer: "&&",
+    },
+    {
+        question: "Which is the correct way to display myVariable in console?",
+        choice: ["console.log(myVariable)", "console.show(myVariable)", "console.display(myVariable)", "console.write(myVariable)"],
+        answer: "console.log(myVariable)",
+    }
+];
